e997ee4230a576f91db775e103ef6f1a1a52267b
minkhantkk2014@gmail.com <mkkk1331997>
2018-06-20 14:29:08 +0630

    gg

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
-import 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite://')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    password = Column(String(12))
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+            self.name, self.fullname, self.password)
+
+
+Base.metadata.create_all(engine)
+ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
+session.add(ed_user)
+our_user = session.query(User).filter_by(name='ed').first()
+print(our_user is ed_user)
